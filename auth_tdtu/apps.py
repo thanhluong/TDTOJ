08a4c96429fe57c4f0afc3155fee2fa6cc5da1b8
thanhluong <doannguyenthanhluong@gmail.com>
2025-02-14 14:59:07 +0700

    Update apps.py path

---
+++ auth_tdtu/apps.py
@@ -0,0 +1,9 @@
+import os
+
+from dmoj.settings import BASE_DIR
+
+
+class AuthTdtUAppConfig(AppConfig):
+    name = 'auth_tdtu'
+    verbose_name = gettext_lazy('TDTU Authentication')
+    path = BASE_DIR
--- judge/apps.py
+++ judge/apps.py
@@ -2,16 +2,6 @@ from django.apps import AppConfig
 from django.db import DatabaseError
 from django.utils.translation import gettext_lazy
 
-import os
-
-from dmoj.settings import BASE_DIR
-
-
-class AuthTdtUAppConfig(AppConfig):
-    name = 'auth_tdtu'
-    verbose_name = gettext_lazy('TDTU Authentication')
-    path = BASE_DIR
-
 
 class JudgeAppConfig(AppConfig):
     name = 'judge'
